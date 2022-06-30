6288acba8d1c10365de6af0256d2d17d6d4a0164
Nuo Xu <nuox@usc.edu>
2022-06-30 09:02:17 +0800

    Update emqx_kafka_bridge.erl

--- src/emqx_kafka_bridge.erl
+++ src/emqx_kafka_bridge.erl
@@ -24,6 +24,29 @@
 
 -define(APP, emqx_kafka_bridge).
 
+-export([on_client_connected/4, on_client_disconnected/3]).
+-export([on_client_subscribe/3, on_client_unsubscribe/3]).
+-export([on_session_created/3, on_session_resumed/3, on_session_terminated/3]).
+-export([on_session_subscribed/4, on_session_unsubscribed/4]).
+-export([on_message_publish/2, on_message_delivered/3, on_message_acked/3, on_message_dropped/3]).
+
+%% Called when the plugin application start
+load(Env) ->
+    brod_init([Env]),
+    emqx:hook('client.connected', fun ?MODULE:on_client_connected/4, [Env]),
+    emqx:hook('client.disconnected', fun ?MODULE:on_client_disconnected/3, [Env]),
+    emqx:hook('client.subscribe', fun ?MODULE:on_client_subscribe/3, [Env]),
+    emqx:hook('client.unsubscribe', fun ?MODULE:on_client_unsubscribe/3, [Env]),
+    emqx:hook('session.created', fun ?MODULE:on_session_created/3, [Env]),
+    emqx:hook('session.resumed', fun ?MODULE:on_session_resumed/3, [Env]),
+    emqx:hook('session.subscribed', fun ?MODULE:on_session_subscribed/4, [Env]),
+    emqx:hook('session.unsubscribed', fun ?MODULE:on_session_unsubscribed/4, [Env]),
+    emqx:hook('session.terminated', fun ?MODULE:on_session_terminated/3, [Env]),
+    emqx:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]),
+    emqx:hook('message.delivered', fun ?MODULE:on_message_delivered/3, [Env]),
+    emqx:hook('message.acked', fun ?MODULE:on_message_acked/3, [Env]),
+    emqx:hook('message.dropped', fun ?MODULE:on_message_dropped/3, [Env]).
+
 %% Client Lifecircle Hooks
 -export([ on_client_connect/3
         , on_client_connack/4
